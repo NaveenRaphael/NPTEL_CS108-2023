53089443c2d0ad4af856175caab40c38174683ef
Naveen Raphael <naveenbraphael@gmail.com>
2023-10-17 19:53:46 +0530

    Week 12 - final
    Week 12 - final

--- Week 12/tkinter_calculator.py
+++ Week 12/tkinter_calculator.py
@@ -2,6 +2,13 @@ import tkinter as tk
 from tkinter import ttk
 
 
+def press_eq():
+    '''
+    Resets the view in the calculator
+    '''
+    text.set("")
+
+
 window = tk.Tk()
 
 text = tk.StringVar(value="Initial value")
@@ -9,9 +16,11 @@ show_region = tk.Label(window, textvariable=text)
 
 button_region = tk.Frame(window)
 
-number_buttons = [ttk.Button(button_region, text=f"{i}") for i in range(10)]
+number_buttons = [ttk.Button(button_region, text=f"{i}", command=lambda i=i: text.set(
+    text.get()+f"{i}")) for i in range(10)]
 
-operation_button = {i: ttk.Button(button_region, text=f"{i}") for i in [
+operation_button = {i: ttk.Button(button_region, text=f"{i}", command=lambda i=i: text.set(
+    text.get()+f"{i}")) for i in [
     ".", "=", "+", "-", "*", r"/"]}
 
 number_buttons[0].grid(row=3, column=1, padx=4, pady=4)
@@ -35,6 +44,9 @@ operation_button[r"/"].grid(row=3, column=0, padx=4, pady=4)
 show_region.grid(row=0, column=0)
 button_region.grid(row=1, column=0)
 
+operation_button["="].configure(command=press_eq)
+
 text.set("why")
 
+window.title("Broken calculator")
 window.mainloop()
