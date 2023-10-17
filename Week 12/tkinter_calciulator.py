44e5d1e4912a00cd1c50d692463eebf6e2d3dbb2
Naveen Raphael <naveenbraphael@gmail.com>
2023-10-17 19:23:37 +0530

    Week 12

---
+++ Week 12/tkinter_calciulator.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+window = tk.Tk()
+
+text = tk.StringVar(value="Initial value")
+show_region = tk.Label(window, textvariable=text)
+
+button_region = tk.Frame(window)
+
+number_buttons = [ttk.Button(button_region, text=f"{i}") for i in range(10)]
+
+operation_button = {i: ttk.Button(button_region, text=f"{i}") for i in [
+    ".", "=", "+", "-", "*", r"/"]}
+
+number_buttons[0].grid(row=3, column=1, padx=4, pady=4)
+number_buttons[1].grid(row=2, column=0, padx=4, pady=4)
+number_buttons[2].grid(row=2, column=1, padx=4, pady=4)
+number_buttons[3].grid(row=2, column=2, padx=4, pady=4)
+number_buttons[4].grid(row=1, column=0, padx=4, pady=4)
+number_buttons[5].grid(row=1, column=1, padx=4, pady=4)
+number_buttons[6].grid(row=1, column=2, padx=4, pady=4)
+number_buttons[7].grid(row=0, column=0, padx=4, pady=4)
+number_buttons[8].grid(row=0, column=1, padx=4, pady=4)
+number_buttons[9].grid(row=0, column=2, padx=4, pady=4)
+
+operation_button["="].grid(row=3, column=3, padx=4, pady=4)
+operation_button["."].grid(row=3, column=2, padx=4, pady=4)
+operation_button["+"].grid(row=0, column=3, padx=4, pady=4)
+operation_button["-"].grid(row=1, column=3, padx=4, pady=4)
+operation_button["*"].grid(row=2, column=3, padx=4, pady=4)
+operation_button[r"/"].grid(row=3, column=0, padx=4, pady=4)
+
+show_region.grid(row=0, column=0)
+button_region.grid(row=1, column=0)
+
+text.set("why")
+
+window.mainloop()
